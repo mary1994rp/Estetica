f7dbea61ec5a094e11197d0826d6c1eec554e7d4
esteban2322 <37229509+esteban2322@users.noreply.github.com>
2018-03-28 01:05:48 -0600

    Add files via upload

---
+++ Estetica/src/mx/uam/ayd/estetica/presentacion/ControlConsultarProveedor.java
@@ -0,0 +1,29 @@
+package mx.uam.ayd.estetica.presentacion;
+
+
+import mx.uam.ayd.estetica.modelo.Proveedor;
+import mx.uam.ayd.estetica.negocio.ServicioCuenta;
+import mx.uam.ayd.estetica.negocio.ServicioEmpleado;
+import mx.uam.ayd.estetica.negocio.ServicioProveedor;
+
+public class ControlConsultarProveedor {
+
+	private ServicioProveedor servicioProveedor;
+
+	public ControlConsultarProveedor(ServicioProveedor servicioProveedor) {
+		this.servicioProveedor= servicioProveedor;
+	}
+	
+	public void inicia() {
+	/******* Inicia el caso de uso Consulta proveedor***********/
+		VentanaConsultaProveedor VCP = new VentanaConsultaProveedor(this);
+		VCP.setVisible(true);		
+	}
+	
+	//dame citas
+	public Proveedor[] dameProveedor() {
+		return servicioProveedor.dameProveedor();
+	}
+	
+		
+}
